3cb8a77df9589bc504b4328044b9f6e769237d2e
kamran <mohammadkamranahmad786@gmail.com>
2024-07-16 16:45:22 +0000

    solution

---
+++ src/setupTests.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+configure({testIdAttribute: 'data-testid'})
+
+// configure({testIdAttribute: 'testid'})
